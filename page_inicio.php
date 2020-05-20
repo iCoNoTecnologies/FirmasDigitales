52293c1e0520ed594bc3354b182d320655718a58
JhonyRayck <65360325+JhonyRayck@users.noreply.github.com>
2020-05-19 23:00:36 -0500

    test del la edicion

--- page_inicio.php
+++ page_inicio.php
@@ -6,23 +6,23 @@ $inicio="INICIO";
 
 <html>
 <head>
-	
+
 </head>
 <style type="text/css">
-	
+
 	.transparente{
-    
+
 background:darkslategray;
 width:800px;
 height:650px;
 opacity: 0.8;
-margin-top:20px; 
+margin-top:20px;
 margin-left: 10px;
 position:absolute;
 -moz-opacity: 0.8;
 filter: alpha(opacity=80);
 -khtml-opacity: 0.8;
-    
+
 border-radius: 10px 10px 10px 10px;
 -moz-border-radius: 10px 10px 10px 10px;
 -webkit-border-radius: 10px 10px 10px 10px;
@@ -31,9 +31,7 @@ border: 0px solid #000000;
 }
 </style>
 <body>
-   
-<h1>hola mundo</h1>
+
+<h1>Puto el que lo lea</h1>
 </body>
 </html>
-
-
